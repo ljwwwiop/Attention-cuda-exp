7ae15bc463bb8352bbf2dfc55e68c838ce4a272c
lian <lian@MacBook-Pro.lan>
2025-09-03 11:55:48 +0800

    update flash attention minial cuda v1-version

---
+++ flash-attn-v1-minial/main.cpp
@@ -0,0 +1,8 @@
+#include <torch/extension.h>
+
+// define head
+torch::Tensor forward(torch::Tensor q, torch::Tensor k, torch::Tensor v);
+
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m){
+    m.def("forward", torch::wrap_pybind_function(forward), "forward");
+}
